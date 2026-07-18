9e8739893725e2589a3320668b9aec3501638c7f
Sangita Bera <sangitabera345@gmail.com>
2026-07-18 20:44:16 +0530

    updated schemas.py

--- dataset/schemas.py
+++ dataset/schemas.py
@@ -1,22 +1,15 @@
 from dataclasses import dataclass
 from typing import List
 
-@dataclass
+@dataclass(slots=True)
 class Entity:
     start : int
     end : int
     text : str
     label : str
 
-
-@dataclass
-class Token:
-    text : str
-    start : int
-    end : int
-
     
-@dataclass
+@dataclass(slots=True)
 class Document:
     document_id : str
     text : str
