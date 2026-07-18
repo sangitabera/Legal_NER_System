5805ea6174db27664d5671f860c27456ac8f37db
Sangita Bera <sangitabera345@gmail.com>
2026-07-18 20:19:17 +0530

    updated constants.py

---
+++ configs/constants.py
@@ -0,0 +1,30 @@
+# BIO Tag Prefixes
+B_PREFIX = "B"
+I_PREFIX = "I"
+OUTSIDE_TAG = "O"
+
+
+# Special Label
+IGNORE_INDEX = -100
+
+
+# Supported Dataset Extensions
+JSON_EXTENSION = ".json"
+TEXT_EXTENSION = ".txt"
+
+
+# Encoding
+DEFAULT_ENCODING = "utf-8"
+
+# Logging
+LOGGER_NAME = "legal_ner"
+
+# Randomness
+DEFAULT_RANDOM_SEED = 42
+
+# Special Tokens
+PAD_TOKEN = "[PAD]"
+UNK_TOKEN = "[UNK]"
+CLS_TOKEN = "[CLS]"
+SEP_TOKEN = "[SEP]"
+MASK_TOKEN = "[MASK]"
